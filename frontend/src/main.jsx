23371789150ee69ea8ff46879101aa6f3f42683b
Radlyx25 <nadzimghaz@gmail.com>
2025-05-22 14:09:51 +0800

    Set BrowserRouter basename for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/growthops-2025">
         <App />
       </BrowserRouter>
   </StrictMode>
